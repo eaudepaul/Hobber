0b6a87a8316ea5f209a8a50b1f268950d7094e0a
Paul Daly <pauldaly1992@gmail.com>
2023-06-06 17:38:40 +0200

    efvef e

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root "user_matches#new"
+  root "pages#index"
 
   resources :users, only: [:show] do
     resources :user_matches, only: %i[new create]
