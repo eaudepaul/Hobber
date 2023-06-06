cf1f182cbced7d43c0f62d850994be0e377e21ed
Paul Daly <128895388+eaudepaul@users.noreply.github.com>
2023-06-06 17:10:55 +0200

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def index
-    redirect_to new_user_user_match_path(user_id: User.all.sample.id) if user_signed_in?
+    # just to maintain functionality, readd once views have progressed
+    # redirect_to new_user_user_match_path(user_id: User.all.sample.id) if user_signed_in?
   end
 end
