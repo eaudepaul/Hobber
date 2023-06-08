2362fca7594a37f6b3f248b44456585d3bb18e3e
Charlotte de Janzé <130290009+dejanze@users.noreply.github.com>
2023-06-08 11:33:12 +0200

    cloudinary install

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :username, :bio, :address, :age)
+    params.require(:user).permit(:email, :username, :bio, :address, :age, :photo)
   end
 end
