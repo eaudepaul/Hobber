a0a1925d637c9adc88eee672a4599b380846353a
antonio celano <antonio_celano@hotmail.com>
2023-06-08 11:52:06 +0200

    Refactor

--- app/controllers/user_matches_controller.rb
+++ app/controllers/user_matches_controller.rb
@@ -4,22 +4,23 @@ class UserMatchesController < ApplicationController
   end
 
   def new
-    @new_form = Match.where(secondary_user: current_user).exists?
     @potential_match = User.all.sample
     @user_match = UserMatch.new
   end
 
   def create
-
-    match = Match.create!(secondary_user_id: params[:user_match][:match_id])
     # TODO: check if UserMatch already exists, otherwise create it. Status will be either pending (if the current user liked the proposed user) or declined
-    UserMatch.create!(user_id: current_user.id, match_id: match.id, status: params[:status])
+    match = Match.create!(secondary_user_id: params[:user_match][:match_id])
+    user_match = UserMatch.new(user_match_params)
+    user_match.user_id = current_user.id
+    user_match.match_id = match.secondary_user_id
+    user_match.save
     redirect_to new_user_match_path
   end
 
   private
 
   def user_match_params
-    params.require(:user_match).permit(:user_id, :match_id, :status)
+    params.require(:user_match).permit(:status)
   end
 end
