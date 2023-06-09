cad29111deaccf1a72be5e88470987a3c73068ea
Charlotte de Janzé <130290009+dejanze@users.noreply.github.com>
2023-06-09 11:20:46 +0200

    test page empty working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :test
+
+  def test
+  end
+
   def index
     # just to maintain functionality, readd once views have progressed
     # redirect_to new_user_user_match_path(user_id: User.all.sample.id) if user_signed_in?
--- config/routes.rb
+++ config/routes.rb
@@ -8,31 +8,32 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root "pages#index"
-
+  
   resources :user_matches, only: %i[update index new create] do
     resources :appointments, only: %i[new create]
   end
-
+  
   resources :chatrooms, only: %i[show index create destroy] do
     resources :messages, only: :create
   end
-
+  
   resources :users, only: %i[new edit update]
-
+  
   resources :appointments, only: %i[show edit] do
     resources :reviews, only: %i[new create]
   end
-
+  
   resources :appointments, only: %i[index destroy]
-
+  
   resources :games, only: %i[index new create] do
     member do
       put :update_popularity
     end
     resources :user_games, only: %i[create new index]
   end
-
+  
   authenticate :user, ->(user) { user.admin? } do
     mount Blazer::Engine, at: "blazer"
   end
+  get "test", to: "pages#test", as: :test
 end
