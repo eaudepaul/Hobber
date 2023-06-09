c44104761407c1fd5c1fee19827870693a6dd49c
Katharine Lin <katharine.exe@gmail.com>
2023-06-09 15:26:10 +0200

    building blocks for cards on appointment index

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,16 +1,14 @@
 class AppointmentsController < ApplicationController
   def index
-    @appointments = Appointment.all
+    @user_matches = UserMatch.joins(:match).where("user_matches.status = 'approved' AND (user_matches.user_id = :user OR matches.secondary_user_id = :user)", user: current_user.id)
   end
 
   def new
     @user_match = UserMatch.find(params[:user_match_id])
     @appointment = Appointment.new
-  
   end
 
   def show
-    
     @game = Appointment.new
   end
 
