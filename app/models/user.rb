c06cffe95b10c1120867dd60b76911a77af3614b
Paul Daly <pauldaly1992@gmail.com>
2023-06-12 15:53:17 +0200

    afternoon3

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
   has_one_attached :photo
   has_many :user_matches
   has_many :matches, through: :user_matches
@@ -10,5 +10,5 @@ class User < ApplicationRecord
   has_many :user_games
   has_many :games, through: :user_games
   validates :email, uniqueness: true
-  validates :username, uniqueness: true
+  # validates :username, uniqueness: true
 end
