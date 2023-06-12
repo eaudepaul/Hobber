068b3149d4d959af80dd6a8779c7de805f69cbac
Paul Daly <pauldaly1992@gmail.com>
2023-06-12 11:10:37 +0200

    navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[destroy edit update show]
+  before_action :set_user, only: %i[destroy edit update]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user.id
     @chatroom = Chatroom.new
   end
 
