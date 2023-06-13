34ec0a274f4c9a0ba3fdd28deb36bc0d53b478d3
Charlotte de Janzé <130290009+dejanze@users.noreply.github.com>
2023-06-13 12:46:13 +0200

    chatrooms styling

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,202 +10,201 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_609_130_339) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_130339) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum'
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'appointments', force: :cascade do |t|
-    t.string 'address'
-    t.date 'date'
-    t.time 'start_time'
-    t.time 'end_time'
-    t.bigint 'game_id', null: false
-    t.bigint 'user_match_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'status', default: 'pending'
-    t.index ['game_id'], name: 'index_appointments_on_game_id'
-    t.index ['user_match_id'], name: 'index_appointments_on_user_match_id'
+  create_table "appointments", force: :cascade do |t|
+    t.string "address"
+    t.date "date"
+    t.time "start_time"
+    t.time "end_time"
+    t.bigint "game_id", null: false
+    t.bigint "user_match_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status", default: "pending"
+    t.index ["game_id"], name: "index_appointments_on_game_id"
+    t.index ["user_match_id"], name: "index_appointments_on_user_match_id"
   end
 
-  create_table 'blazer_audits', force: :cascade do |t|
-    t.bigint 'user_id'
-    t.bigint 'query_id'
-    t.text 'statement'
-    t.string 'data_source'
-    t.datetime 'created_at'
-    t.index ['query_id'], name: 'index_blazer_audits_on_query_id'
-    t.index ['user_id'], name: 'index_blazer_audits_on_user_id'
+  create_table "blazer_audits", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "query_id"
+    t.text "statement"
+    t.string "data_source"
+    t.datetime "created_at"
+    t.index ["query_id"], name: "index_blazer_audits_on_query_id"
+    t.index ["user_id"], name: "index_blazer_audits_on_user_id"
   end
 
-  create_table 'blazer_checks', force: :cascade do |t|
-    t.bigint 'creator_id'
-    t.bigint 'query_id'
-    t.string 'state'
-    t.string 'schedule'
-    t.text 'emails'
-    t.text 'slack_channels'
-    t.string 'check_type'
-    t.text 'message'
-    t.datetime 'last_run_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_checks_on_creator_id'
-    t.index ['query_id'], name: 'index_blazer_checks_on_query_id'
+  create_table "blazer_checks", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.bigint "query_id"
+    t.string "state"
+    t.string "schedule"
+    t.text "emails"
+    t.text "slack_channels"
+    t.string "check_type"
+    t.text "message"
+    t.datetime "last_run_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_checks_on_creator_id"
+    t.index ["query_id"], name: "index_blazer_checks_on_query_id"
   end
 
-  create_table 'blazer_dashboard_queries', force: :cascade do |t|
-    t.bigint 'dashboard_id'
-    t.bigint 'query_id'
-    t.integer 'position'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['dashboard_id'], name: 'index_blazer_dashboard_queries_on_dashboard_id'
-    t.index ['query_id'], name: 'index_blazer_dashboard_queries_on_query_id'
+  create_table "blazer_dashboard_queries", force: :cascade do |t|
+    t.bigint "dashboard_id"
+    t.bigint "query_id"
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dashboard_id"], name: "index_blazer_dashboard_queries_on_dashboard_id"
+    t.index ["query_id"], name: "index_blazer_dashboard_queries_on_query_id"
   end
 
-  create_table 'blazer_dashboards', force: :cascade do |t|
-    t.bigint 'creator_id'
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_dashboards_on_creator_id'
+  create_table "blazer_dashboards", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_dashboards_on_creator_id"
   end
 
-  create_table 'blazer_queries', force: :cascade do |t|
-    t.bigint 'creator_id'
-    t.string 'name'
-    t.text 'description'
-    t.text 'statement'
-    t.string 'data_source'
-    t.string 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['creator_id'], name: 'index_blazer_queries_on_creator_id'
+  create_table "blazer_queries", force: :cascade do |t|
+    t.bigint "creator_id"
+    t.string "name"
+    t.text "description"
+    t.text "statement"
+    t.string "data_source"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_blazer_queries_on_creator_id"
   end
 
-  create_table 'chatrooms', force: :cascade do |t|
-    t.bigint 'user_match_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'name'
-    t.index ['user_match_id'], name: 'index_chatrooms_on_user_match_id'
+  create_table "chatrooms", force: :cascade do |t|
+    t.bigint "user_match_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["user_match_id"], name: "index_chatrooms_on_user_match_id"
   end
 
-  create_table 'games', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'matches', force: :cascade do |t|
-    t.integer 'secondary_user_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "matches", force: :cascade do |t|
+    t.integer "secondary_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'messages', force: :cascade do |t|
-    t.string 'content'
-    t.bigint 'chatroom_id', null: false
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['chatroom_id'], name: 'index_messages_on_chatroom_id'
-    t.index ['user_id'], name: 'index_messages_on_user_id'
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
-  create_table 'pages', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "pages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.string 'content'
-    t.integer 'rating'
-    t.bigint 'appointment_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['appointment_id'], name: 'index_reviews_on_appointment_id'
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "appointment_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["appointment_id"], name: "index_reviews_on_appointment_id"
   end
 
-  create_table 'user_games', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'game_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['game_id'], name: 'index_user_games_on_game_id'
-    t.index ['user_id'], name: 'index_user_games_on_user_id'
+  create_table "user_games", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_user_games_on_game_id"
+    t.index ["user_id"], name: "index_user_games_on_user_id"
   end
 
-  create_table 'user_matches', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.bigint 'match_id', null: false
-    t.string 'status'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['match_id'], name: 'index_user_matches_on_match_id'
-    t.index ['user_id'], name: 'index_user_matches_on_user_id'
+  create_table "user_matches", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "match_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["match_id"], name: "index_user_matches_on_match_id"
+    t.index ["user_id"], name: "index_user_matches_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'username'
-    t.integer 'age'
-    t.string 'bio'
-    t.string 'address'
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.boolean 'admin', default: false, null: false
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "username"
+    t.integer "age"
+    t.string "bio"
+    t.string "address"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'appointments', 'games'
-  add_foreign_key 'appointments', 'user_matches'
-  add_foreign_key 'chatrooms', 'user_matches'
-  add_foreign_key 'matches', 'users', column: 'secondary_user_id'
-  add_foreign_key 'messages', 'chatrooms'
-  add_foreign_key 'messages', 'users'
-  add_foreign_key 'reviews', 'appointments'
-  add_foreign_key 'reviews', 'users'
-  add_foreign_key 'user_games', 'games'
-  add_foreign_key 'user_games', 'users'
-  add_foreign_key 'user_matches', 'matches'
-  add_foreign_key 'user_matches', 'users'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "appointments", "games"
+  add_foreign_key "appointments", "user_matches"
+  add_foreign_key "chatrooms", "user_matches"
+  add_foreign_key "matches", "users", column: "secondary_user_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "reviews", "appointments"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "user_games", "games"
+  add_foreign_key "user_games", "users"
+  add_foreign_key "user_matches", "matches"
+  add_foreign_key "user_matches", "users"
 end
