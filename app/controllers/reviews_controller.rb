cd44fd03509d0b178c34ee3e2e2b212db3f32dff
antonio celano <antonio_celano@hotmail.com>
2023-06-14 18:02:12 +0200

    Set @reviews in index method inside reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,7 +4,9 @@
 class ReviewsController < ApplicationController
   def index
     # Reviews that current user received: this finds all the reviews left by a secondary_user for the current user, who is defined in a Match instance
-    @reviews = Review.where(user_id: current_user.matches.pluck(:secondary_user_id))
+    user = User.find(params[:user_id])
+    @reviews = user.appointments.map { |appointment| appointment.reviews.where.not(user:) }.flatten
+    raise
   end
 
   def show
